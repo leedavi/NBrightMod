2358b4f4a369cc67989743d93a9d010a27e43a7d
leedavi <dcl@nevoweb.com>
2015-11-03 23:10:41 +0100

    contact form

--- render/NBrightModRazorTokens.cs
+++ render/NBrightModRazorTokens.cs
@@ -79,6 +79,42 @@ namespace NBrightMod.render
             return new RawString(strOut);
         }
 
+        public IEncodedString ThemeSelectList(NBrightInfo info, String xpath, String relitiveRootFolder, String attributes = "", Boolean allowEmpty = true)
+        {
+            if (attributes.StartsWith("ResourceKey:")) attributes = ResourceKey(attributes.Replace("ResourceKey:", "")).ToString();
+
+            var mappathRootFolder = System.Web.Hosting.HostingEnvironment.MapPath(relitiveRootFolder);
+            var dirlist = System.IO.Directory.GetDirectories(mappathRootFolder);
+            var tList = new List<String>();
+            foreach (var d in dirlist)
+            {
+                var dr = new System.IO.DirectoryInfo(d);
+                tList.Add(dr.Name);
+            }
+            var strOut = "";
+
+            var upd = getUpdateAttr(xpath, attributes);
+            var id = xpath.Split('/').Last();
+            strOut = "<select id='" + id + "' " + upd + " " + attributes + ">";
+            var c = 0;
+            var s = "";
+            if (allowEmpty) strOut += "    <option value=''></option>";
+            foreach (var tItem in tList)
+            {
+                if (tItem.ToLower() != "shared" && tItem.ToLower() != "config")
+                {
+                    if (info.GetXmlProperty(xpath) == tItem)
+                        s = "selected";
+                    else
+                        s = "";
+                    strOut += "    <option value='" + tItem + "' " + s + ">" + tItem + "</option>";
+                }
+            }
+            strOut += "</select>";
+
+            return new RawString(strOut);
+        }
+
     }
 
 
